7f4d143d5553ab122704a8c6757b8516404a0206
cashmerewool <67176000+cashmerewool@users.noreply.github.com>
2023-01-28 20:14:21 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,6 +4,7 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
+    //successful input
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
@@ -20,7 +21,7 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input4);
     assertArrayEquals(new int[] {1, 2, 2, 1}, input4);
 
-    //faiure inducing
+    //faiure-inducing input
     int[] input5 = {1, 2, 3, 4, 5};
     ArrayExamples.reverseInPlace(input5);
     assertArrayEquals(new int[] {5, 4, 3, 2, 1}, input5);
@@ -35,16 +36,23 @@ public class ArrayTests {
 
   @Test
   public void testReversed() {
+    //successful input
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    //failure-inducing input
+    int[] input2 = {1, 3, 5, 2};
+    assertArrayEquals(new int[] {2, 5, 3, 1}, ArrayExamples.reversed(input2));
   }
 
   @Test
   public void testAverageWithoutLowest() {
+    //successful input
     double[] input1 = {1, 2, 3, 4, 5, 6};
     assertEquals(4.0, 
         ArrayExamples.averageWithoutLowest(input1), 0.001);
 
+    //failure-inducing
     double[] input2 = {1, 1, 2, 3, 4, 5, 6};
     assertEquals(4.0, 
         ArrayExamples.averageWithoutLowest(input2), 0.001);
