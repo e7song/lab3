c76bdff0df63b3ccffe25c0dbfde75923747966c
cashmerewool <67176000+cashmerewool@users.noreply.github.com>
2023-01-28 20:51:24 -0800

    averageWithoutLowest fix

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -29,6 +29,7 @@ public class ArrayExamples {
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
+    int numRemoved = 0;
     if(arr.length < 2) { return 0.0; }
     double lowest = arr[0];
     for(double num: arr) {
@@ -37,8 +38,11 @@ public class ArrayExamples {
     double sum = 0;
     for(double num: arr) {
       if(num != lowest) { sum += num; }
+      else {
+        numRemoved++;
+      }
     }
-    return sum / (arr.length - 1);
+    return sum / (arr.length - numRemoved);
   }
 
 
